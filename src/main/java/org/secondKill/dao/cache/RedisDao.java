a63c5a5f82f83b707e0631379b803d1c6c987903
flyingseal66 <flyingseal66@gmail.com>
2019-01-21 09:30:29 +0000

    修改依赖版本，代码微调优化

--- src/main/java/org/secondKill/dao/cache/RedisDao.java
+++ src/main/java/org/secondKill/dao/cache/RedisDao.java
@@ -6,7 +6,6 @@ import com.dyuproject.protostuff.runtime.RuntimeSchema;
 import org.secondKill.entity.Seckill;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
